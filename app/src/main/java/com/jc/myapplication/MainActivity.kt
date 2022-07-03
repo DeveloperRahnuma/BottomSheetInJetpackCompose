b250ac887b8b3ffce98082371b27e39d1e0a2d7e
DeveloperRahnuma <rahnuma.sharib786@gmail.com>
2022-07-03 16:23:38 +0530

    initialize commit

--- app/src/main/java/com/jc/myapplication/MainActivity.kt
+++ app/src/main/java/com/jc/myapplication/MainActivity.kt
@@ -5,69 +5,40 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.aspectRatio
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.GridCells
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyVerticalGrid
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.jc.myapplication.ui.theme.MyApplicationTheme
+import kotlinx.coroutines.launch
 
 
+@ExperimentalMaterialApi
 @ExperimentalFoundationApi
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MyApplicationTheme {
-                LazyVerticalGrid(
-                    cells = GridCells.Adaptive(200.dp),
-                    content = {
-                        items(50){it ->
-                            Box(modifier = Modifier
-                                .padding(8.dp)
-                                .aspectRatio(1f)
-                                .background(Color.Red)
-                            )
-                        }
-                    }
-                )
-
-                LazyVerticalGrid(
-                    cells = GridCells.Fixed(4),
-                    content = {
-                        items(50){it ->
-                            Box(modifier = Modifier
-                                .padding(8.dp)
-                                .aspectRatio(1f)
-                                .background(Color.Red)
-                            )
-                        }
-                    }
-                )
+                val windowInfo = rememberWindowInfo()
+                if(windowInfo.screenWidthInfo is WindowInfo.WindowType.Compect){
+                    potraitLayout()
+                }else{
+                    landscapeLayout()
+                }
+               }
             }
         }
     }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    MyApplicationTheme {
-        Greeting("Android")
-    }
-}
---
+++ app/src/main/java/com/jc/myapplication/WindowInfo.kt
@@ -0,0 +1,39 @@
+package com.jc.myapplication
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun rememberWindowInfo() : WindowInfo{
+    val configuration = LocalConfiguration.current
+return WindowInfo(
+    screenWidthInfo = when{
+        configuration.screenWidthDp < 600 -> WindowInfo.WindowType.Compect
+        configuration.screenWidthDp < 840 -> WindowInfo.WindowType.Medium
+        else -> WindowInfo.WindowType.Large
+    },
+    screenHeightInfo = when{
+        configuration.screenHeightDp < 480 -> WindowInfo.WindowType.Compect
+        configuration.screenHeightDp < 900 -> WindowInfo.WindowType.Medium
+        else -> WindowInfo.WindowType.Large
+    },
+    screenWidth = configuration.screenWidthDp.dp,
+    screenHeight = configuration.screenHeightDp.dp
+)
+}
+
+data class WindowInfo(
+    val screenWidthInfo: WindowType,
+    val screenHeightInfo: WindowType,
+    val screenWidth : Dp,
+    val screenHeight : Dp,
+    ){
+    sealed class WindowType(){
+        object Compect : WindowType()
+        object Medium : WindowType()
+        object Large : WindowType()
+
+    }
+}
